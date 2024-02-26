838e6adeea746d7bc5203a92cae4b923dd7d55f1
Vladislav Pikelniy <vladpikeniy@gmail.com>
2024-02-26 18:00:25 +0200

    Home work 1

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
-let hours = prompt("Введіть кількість годин");
-let seconds = hours * 3600;
-
-alert(`У ${hours} годинах: ${seconds} секунд`);
+alert('Hello User')
+console.log('this 1');
+let answer1 = prompt('How are you', 'def');
+console.log('this 2', answer1);
+confirm('');
+console.log('this 3');
