d0edce059eb3b06130cdd05c51d8ff080f7b9624
Vladislav Pikelniy <vladpikeniy@gmail.com>
2024-02-26 18:52:23 +0200

    Home work 5

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let num = parseFloat(prompt("Введите первое число:"));
+let num1 = parseFloat(prompt("Введите второе число:"));
+let num2 = parseFloat(prompt("Введите третье число:"));
+
+let averege = (num + num1 + num2) / 3;
+
+alert("Среднее арифметическое трех чисел: " + averege);
