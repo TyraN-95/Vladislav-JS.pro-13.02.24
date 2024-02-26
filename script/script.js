306c7cda267a39a0dad910f9717fa6b94631723e
Vladislav Pikelniy <vladpikeniy@gmail.com>
2024-02-26 16:44:59 +0200

    Home work 3

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let num = prompt(`Введите любое число`);
+let num1 = prompt(`Введите любое число`);
+
+let plus = +num + +num1;
+let minus = num - num1;
+let div = num / num1;
+let multi = num * num1;
+
+alert(`${num} + ${num1} = ${plus}\n ${num} - ${num1} = ${minus}\n ${num} / ${num1} = ${div}\n ${num} * ${num1} = ${multi}`);
